9b1e41baeb1680dcc00e8060415d00c7405a3c33
HerbertHsu <herbert.c.hsu@gmail.com>
2021-05-26 22:43:53 +0800

    add HasAtLeastOneDigitTest

---
+++ PasswordValidationTests/HasAtLeastOneDigitTest.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using PasswordValidation.Rules.Interfaces;
+using PasswordValidation.Rules.Password;
+using Xunit;
+
+namespace PasswordValidationTests
+{
+    public class HasAtLeastOneDigitTest
+    {
+        private readonly ISpecification<string> _hasAtLeastOneDigitSpecification;
+
+        public HasAtLeastOneDigitTest()
+        {
+            _hasAtLeastOneDigitSpecification = new HasAtLeastOneDigit();
+        }
+
+        /// <summary>
+        /// password: abcdef
+        /// expect: fail
+        /// </summary>
+        [Fact]
+        public void Password_Doesnt_Have_Any_Digit_Character_Should_Be_Invalid()
+        {
+            var actualResult = _hasAtLeastOneDigitSpecification.IsSatisfiedBy("abcdef");
+            actualResult.Should().BeFalse();
+        }
+        
+        /// <summary>
+        /// password: abc123def
+        /// expect: pass
+        /// </summary>
+        [Fact]
+        public void Password_Does_Have_Multiple_Digit_Characters_Should_Be_Valid()
+        {
+            var actualResult = _hasAtLeastOneDigitSpecification.IsSatisfiedBy("abc123def");
+            actualResult.Should().BeTrue();
+        }
+        
+        /// <summary>
+        /// password: abc1def
+        /// expect: pass
+        /// </summary>
+        [Fact]
+        public void Password_Does_Only_Have_One_Digit_Character_Should_Be_Valid()
+        {
+            var actualResult = _hasAtLeastOneDigitSpecification.IsSatisfiedBy("abc1def");
+            actualResult.Should().BeTrue();
+        }
+    }
+}

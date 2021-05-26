d4c674ffb3819e6daeb94616c1a1f471e708eb60
HerbertHsu <herbert.c.hsu@gmail.com>
2021-05-26 23:04:56 +0800

    add LowercaseLettersOnlyTest

---
+++ PasswordValidationTests/LowercaseLettersOnlyTest.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using PasswordValidation.Rules.Interfaces;
+using PasswordValidation.Rules.Password;
+using Xunit;
+
+namespace PasswordValidationTests
+{
+    public class LowercaseLettersOnlyTest
+    {
+        private readonly ISpecification<string> _lowercaseLettersOnlySpecification;
+
+        public LowercaseLettersOnlyTest()
+        {
+            _lowercaseLettersOnlySpecification = new LowercaseLettersOnly();
+        }
+
+        /// <summary>
+        /// password: ABCDEF123
+        /// expect: fail
+        /// </summary>
+        [Fact]
+        public void Password_Has_All_Uppercase_Letters_Should_Be_Invalid()
+        {
+            var actualResult = _lowercaseLettersOnlySpecification.IsSatisfiedBy("ABCDEF123");
+            actualResult.Should().BeFalse();
+        }
+        
+        /// <summary>
+        /// password: abcdef123
+        /// expect: pass
+        /// </summary>
+        [Fact]
+        public void Password_Has_All_Lowercase_Letters_Should_Be_Valid()
+        {
+            var actualResult = _lowercaseLettersOnlySpecification.IsSatisfiedBy("abcdef123");
+            actualResult.Should().BeTrue();
+        }
+        
+        /// <summary>
+        /// password: abcDEF123
+        /// expect: fail
+        /// </summary>
+        [Fact]
+        public void Password_Has_Both_Uppercase_And_Lowercase_Letters_Should_Be_Invalid()
+        {
+            var actualResult = _lowercaseLettersOnlySpecification.IsSatisfiedBy("abcDEF123");
+            actualResult.Should().BeFalse();
+        }
+    }
+}

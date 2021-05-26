1efd0a995933c641f9332d4f38861b8f9d536764
HerbertHsu <herbert.c.hsu@gmail.com>
2021-05-26 13:13:39 +0800

    add LengthMinimumFiveMaximumTwelveTest

---
+++ PasswordValidationTests/LengthMinimumFiveMaximumTwelveTest.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using PasswordValidation.Rules.Interfaces;
+using PasswordValidation.Rules.Password;
+using Xunit;
+
+namespace PasswordValidationTests
+{
+    public class LengthMinimumFiveMaximumTwelveTest
+    {
+        private readonly ISpecification<string> _lengthMinimumFiveMaximumTwelveSpecification;
+        
+        public LengthMinimumFiveMaximumTwelveTest()
+        {
+            _lengthMinimumFiveMaximumTwelveSpecification = new LengthMinimumFiveMaximumTwelve();
+        }
+
+        /// <summary>
+        /// password: word1
+        /// expect: pass
+        /// </summary>
+        [Fact]
+        public void Password_Length_Is_Five_Should_Be_Valid()
+        {
+            var actualResult = _lengthMinimumFiveMaximumTwelveSpecification.IsSatisfiedBy("word1");
+            actualResult.Should().BeTrue();
+        }
+        
+        /// <summary>
+        /// password: password1234
+        /// expect: pass
+        /// </summary>
+        [Fact]
+        public void Password_Length_Is_Twelve_Should_Be_Valid()
+        {
+            var actualResult = _lengthMinimumFiveMaximumTwelveSpecification.IsSatisfiedBy("password1234");
+            actualResult.Should().BeTrue();
+        }
+        
+        /// <summary>
+        /// password: a1
+        /// expect: fail
+        /// </summary>
+        [Fact]
+        public void Password_Length_Is_Less_Than_Five_Should_Be_Fail()
+        {
+            var actualResult = _lengthMinimumFiveMaximumTwelveSpecification.IsSatisfiedBy("a1");
+            actualResult.Should().BeFalse();
+        }
+        
+        /// <summary>
+        /// password: abcdefgh12345678
+        /// expect: fail
+        /// </summary>
+        [Fact]
+        public void Password_Length_Is_More_Than_Twelve_Should_Be_Fail()
+        {
+            var actualResult = _lengthMinimumFiveMaximumTwelveSpecification.IsSatisfiedBy("abcdefgh12345678");
+            actualResult.Should().BeFalse();
+        }
+    }
+}

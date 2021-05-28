92f89cfabe5cfd718c59f2fd1eb2ce15143c34bd
HerbertHsu <herbert.c.hsu@gmail.com>
2021-05-28 13:33:16 +0800

    refine PasswordValidator using IHasAtLeastOneDigitRule

--- PasswordValidation/Rules/PasswordValidator.cs
+++ PasswordValidation/Rules/PasswordValidator.cs
@@ -1,17 +1,28 @@
-﻿using PasswordValidation.Specifications.Implementation;
+﻿using PasswordValidation.Rules.Interfaces;
+using PasswordValidation.Specifications.Implementation;
 
 namespace PasswordValidation.Rules
 {
     public class PasswordValidator: Validator<string>
     {
-        public PasswordValidator()
+        public PasswordValidator(IHasAtLeastOneDigitRule hasAtLeastOneDigitRule)
         {
+            Add("ContainsNoDigit", hasAtLeastOneDigitRule);
             Add("ContainsNoLetter", new Rule<string>(new HasAtLeastOneLetter(), "Password should contain at least 1 letter"));
-            Add("ContainsNoDigit", new Rule<string>(new HasAtLeastOneDigit(), "Password should contain at least 1 digit"));
             Add("LowercaseLetterOnly", new Rule<string>(new LowercaseLettersOnly(), "Password should not contain any uppercase letter"));
             Add("LengthMinimumFiveMaximumTwelve", new Rule<string>(new LengthMinimumFiveMaximumTwelve(), "Password Length should be between 5 and 12"));
             Add("ContainsUnnecessaryCharacter", new Rule<string>(new NonSpecialCharacters(), "Password should not contain any non-digit and non-letter character"));
             Add("NotFollowBySameSequenceOfCharacters", new Rule<string>(new NotFollowBySameSequenceOfCharacters(), "Password should not contain any sequence of characters"));
         }
+        
+        // public PasswordValidator()
+        // {
+        //     Add("ContainsNoLetter", new Rule<string>(new HasAtLeastOneLetter(), "Password should contain at least 1 letter"));
+        //     Add("ContainsNoDigit", new Rule<string>(new HasAtLeastOneDigit(), "Password should contain at least 1 digit"));
+        //     Add("LowercaseLetterOnly", new Rule<string>(new LowercaseLettersOnly(), "Password should not contain any uppercase letter"));
+        //     Add("LengthMinimumFiveMaximumTwelve", new Rule<string>(new LengthMinimumFiveMaximumTwelve(), "Password Length should be between 5 and 12"));
+        //     Add("ContainsUnnecessaryCharacter", new Rule<string>(new NonSpecialCharacters(), "Password should not contain any non-digit and non-letter character"));
+        //     Add("NotFollowBySameSequenceOfCharacters", new Rule<string>(new NotFollowBySameSequenceOfCharacters(), "Password should not contain any sequence of characters"));
+        // }
     }
 }
--- PasswordValidationTests/PasswordValidatorTest.cs
+++ PasswordValidationTests/PasswordValidatorTest.cs
@@ -1,7 +1,8 @@
-using System;
 using PasswordValidation.Rules;
 using Xunit;
 using FluentAssertions;
+using PasswordValidation.Rules.Implementations;
+using PasswordValidation.Specifications.Implementation;
 
 namespace PasswordValidationTests
 {
@@ -10,7 +11,7 @@ namespace PasswordValidationTests
         [Fact]
         public void Password_Validate_Success()
         {
-            var actualResult = new PasswordValidator().Validate("a1b2c");
+            var actualResult = new PasswordValidator(new HasAtLeastOneDigitRule(new HasAtLeastOneDigit())).Validate("a1b2c");
             actualResult.IsValid.Should().BeTrue();
             actualResult.Message.Should().BeNullOrEmpty();
         }
@@ -18,7 +19,7 @@ namespace PasswordValidationTests
         [Fact]
         public void Password_Validate_Fail_ShouldAtLeastOneDigit_And_ShouldLowercaseLettersOnly()
         {
-            var actualResult = new PasswordValidator().Validate("AbCdEf");
+            var actualResult = new PasswordValidator(new HasAtLeastOneDigitRule(new HasAtLeastOneDigit())).Validate("AbCdEf");
             actualResult.IsValid.Should().BeFalse();
             actualResult.Errors.Contains(new ValidationError(type: "ContainsNoDigit", "Password should contain at least 1 digit")).Should().BeTrue();
             actualResult.Errors.Contains(new ValidationError(type: "LowercaseLetterOnly", "Password should not contain any uppercase letter")).Should().BeTrue();

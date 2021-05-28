a0408d555c9a747a04041451d0e3d981c57ffd79
HerbertHsu <herbert.c.hsu@gmail.com>
2021-05-28 15:41:11 +0800

    create interface and rule class for LengthMinimumFiveMaximumTwelve

---
+++ PasswordValidation/Rules/Implementations/LengthMinimumFiveMaximumTwelveRule.cs
@@ -0,0 +1,21 @@
+﻿using PasswordValidation.Rules.Interfaces;
+using PasswordValidation.Specifications.Interfaces;
+
+namespace PasswordValidation.Rules.Implementations
+{
+    public class LengthMinimumFiveMaximumTwelveRule : ILengthMinimumFiveMaximumTwelveRule
+    {
+        private readonly ILengthMinimumFiveMaximumTwelve _specification;
+        public string ErrorMessage => "Password Length should be between 5 and 12";
+
+        public LengthMinimumFiveMaximumTwelveRule(ILengthMinimumFiveMaximumTwelve specification)
+        {
+            _specification = specification;
+        }
+        
+        public bool Validate(string entity)
+        {
+            return _specification.IsSatisfiedBy(entity);
+        }
+    }
+}

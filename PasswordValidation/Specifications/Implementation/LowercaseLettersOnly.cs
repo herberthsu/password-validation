ecb2047df72472a55b76d3bef51683e0fa1c4f9f
HerbertHsu <herbert.c.hsu@gmail.com>
2021-05-28 16:15:24 +0800

    add ILowercaseLetterOnly interface and applied to LowercaseLettersOnly specification class

--- PasswordValidation/Specifications/Implementation/LowercaseLettersOnly.cs
+++ PasswordValidation/Specifications/Implementation/LowercaseLettersOnly.cs
@@ -3,7 +3,7 @@ using PasswordValidation.Specifications.Interfaces;
 
 namespace PasswordValidation.Specifications.Implementation
 {
-    public class LowercaseLettersOnly : ISpecification<string>
+    public class LowercaseLettersOnly : ILowercaseLetterOnly
     {
         public bool IsSatisfiedBy(string entity)
         {

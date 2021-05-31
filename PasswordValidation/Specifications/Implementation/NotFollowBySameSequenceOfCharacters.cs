7328c45a4f87ffdb888de418e0cf4a9a56386ef2
HerbertHsu <herbert.c.hsu@gmail.com>
2021-05-31 10:26:18 +0800

    add comments

--- PasswordValidation/Specifications/Implementation/NotFollowBySameSequenceOfCharacters.cs
+++ PasswordValidation/Specifications/Implementation/NotFollowBySameSequenceOfCharacters.cs
@@ -7,16 +7,37 @@ namespace PasswordValidation.Specifications.Implementation
     {
         public bool IsSatisfiedBy(string entity)
         {
+            /*
+            1. converting string to array of characters 
+            2. group the characters and store its indexes 
+            3. filter it by number of stored index is more than 1
+            
+            sample string passpass
+            IGrouping<char, int>
+                [0] Key = p
+                    [0] = 0
+                    [1] = 4
+                [1] Key = a
+                    [0] = 1
+                    [1] = 5
+                [2] Key = s
+                    [0] = 2
+                    [1] = 3
+                    [2] = 6
+                    [3] = 7
+            */
             var dictionary = entity
                 .ToCharArray()
                 .Select((c, i) => new {i, c})
                 .ToLookup(x => x.c, x => x.i)
                 .Where(x => x.Count() > 1);
 
+            // this loop is getting the character and its indexes
             foreach (var elem in dictionary)
             {
                 var indexes = elem.ToArray();
 
+                // this loop is getting the list of indexes
                 for (int i = 0; i < indexes.Length - 1; i++)
                 {
                     var index = indexes[i];
@@ -25,6 +46,10 @@ namespace PasswordValidation.Specifications.Implementation
                     var containsSequence = true;
                     var j = 0;
 
+                    /*
+                    if the 2 characters are right next to each other,
+                    it doesn't considered as containing sequence
+                    */
                     if (nextIndex - (index + j) == 1)
                     {
                         continue;
